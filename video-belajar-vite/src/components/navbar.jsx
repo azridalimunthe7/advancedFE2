29a25656793eb596a1889deeca877b0d3fa8c13d
azridalimunthe7 <azridalimunthe@gmail.com>
2026-01-27 22:42:42 +0700

    fix deploy vercel

--- video-belajar-vite/src/components/navbar.jsx
+++ video-belajar-vite/src/components/navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <nav className="w-full px-6 py-10">
+    <nav className="w-full px-6 py-4">
       <img src="/img/videobelajar.png" alt="Video Belajar" className="w-40" />
     </nav>
   );
--- video-belajar-vite/vite.config.js
+++ video-belajar-vite/vite.config.js
@@ -8,4 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss(), // <--- 2. Panggil tailwind di sini
   ],
+  base: process.env.VITE_BASE_PATH || "/advancedFE2",
 });
